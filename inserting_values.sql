3e86eab8d9980d8f0a4bcf2f8c2006f399278aeb
Amal <amal17ek@gmail.com>
2025-04-12 16:09:41 +0530

    data.sql

---
+++ inserting_values.sql
@@ -0,0 +1,20 @@
+-- Residents
+INSERT INTO residents VALUES (1, 'Ravi', '8248262082', 1);
+INSERT INTO residents VALUES (2, 'Sarh', '9317382947', 2);
+INSERT INTO residents VALUES (3, 'Rajan', '8264727301', 3);
+INSERT INTO residents VALUES (4, 'Manju', '7247402837', 4);
+INSERT INTO residents VALUES (5, 'Ravi B', '9264710473', 2);  -- roommate with Sarh
+
+-- Properties
+INSERT INTO Properties VALUES (1, 'MG Road', 'Apartment', 15000);
+INSERT INTO Properties VALUES (2, 'Koramangala', 'Apartment', 30000);
+INSERT INTO Properties VALUES (3, 'MG Road', 'Cottage', 10000);
+INSERT INTO Properties VALUES (4, 'Whitefield', 'Apartment', 16000);
+
+-- Payments
+INSERT INTO Payments VALUES (1, 1, '2025-04-01', 15000);
+INSERT INTO Payments VALUES (2, 2, '2025-04-01', 30000);
+INSERT INTO Payments VALUES (3, 3, '2025-04-01', 10000);
+INSERT INTO Payments VALUES (4, 4, '2025-04-05', 16000);
+INSERT INTO Payments VALUES (5, 5, '2025-04-01', 15000);
+
